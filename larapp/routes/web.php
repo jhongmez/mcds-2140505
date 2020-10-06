0d4f398feacacacc893dbf334d62615a98d6ba53
ofac <ofaczero@gmail.com>
2020-10-05 19:42:45 -0500

    Errors in register page

--- larapp/routes/web.php
+++ larapp/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use \Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('helloworld', function () {
+    return "<h1>Hello World</h1>";
+});
+
+Route::get('users', function () {
+    dd(App\User::all());
+});
+
+Route::get('user/{id}', function ($id) {
+    dd(App\User::findOrFail($id));
+});
+
+Route::get('challenge', function () {
+    foreach (App\User::all()->take(10) as $user) {
+        $years = Carbon::createFromDate($user->birthdate)->diff()->format('%y years old');
+        $since = Carbon::parse($user->created_at);
+    	$rs[]  = $user->fullname." - ".$years." - created ".$since->diffForHumans();
+    }
+    return view('challenge', ['rs' => $rs]);
+});
+
+Route::get('examples', function () {
+    return view('examples');
+});
+
+Auth::routes();
+
+// Middleware
+Route::get('/{locale}', 'LocaleController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
